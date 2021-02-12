602187cfc31ea7df33e287a772f51e08a691b7e9
Juan Pablo <juanpabloquirosmora@gmail.com>
2021-02-12 00:42:53 -0600

    Clean

--- src/App.jsx
+++ src/App.jsx
@@ -29,29 +29,26 @@ const App = () => {
     const [stack, setStack] = useState("")
 
     const items = words(stack, /[^-^+^*^/]+/g)
-    console.log("Renderización de App", items)
+    
     //operador ternario
     // (esVeradero) ? (resultadoPorverdadero) : (resultadoPorFalso)
-    const value = items.length > 0 ? items[items.length-1] : 0
+    const value = items.length > 0 ? items[items.length-1] : "0"
     return (
     <main className='react-calculator'>
         <Result value={value} />
         <Numbers onClickNumber={number => {
-            console.log("Click en number", number)
+            
             setStack(`${stack}${number}`)
         }}
         />
         <Functions
-            onContentClear={() => {
-                console.log("Content Clear")
-                setStack("")
-            }}
+            onContentClear={() => {setStack("")}}
                 
             onDelete={() => {
                 
                 if(stack.length> 0) {
                     const newStack = stack.substring(0, stack.length - 1)
-                    console.log("onDelete")
+                    
                     setStack(newStack)
 
                 }
@@ -61,21 +58,17 @@ const App = () => {
         />
         <MathOperations 
             onClickOperation={operation => {
-                console.log("Operation:", operation)
+                
                 setStack(`${stack}${operation}`)
             }} 
             onClickEqual={equal=> {
-                console.log("Equal:", equal) 
+                
                 setStack(eval(stack).toString())
 
             }}
         />
     </main>)
-    
-
-
 }
 
 // exportación
-
 export default App
